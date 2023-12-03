b500452646a3d93b3340dfeacc864967a4c6f8f9
nais2008 <134312352+nais2008@users.noreply.github.com>
2023-12-03 14:28:30 +0500

    ending

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-alert('ОМГ, всплавающее окно');
-prompt('Вводи')
+let name = 'Никита'
+let age = 15
+let history = 'Ну по идее я web-программист, но какимто боком я начал делать приложение по обработке фото на python используя PyQT5'
+
+confirm("Да")
+console.log(`Имя: ${name}`)
+console.log(`Возраст: ${age}`)
+console.log(`История: ${history}`)
