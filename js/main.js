ab97b17681d68c84da6af64c83cf4bc2a6ca4fdf
nais2008 <134312352+nais2008@users.noreply.github.com>
2023-12-10 14:42:43 +0500

    end_vtoti_urok

--- js/main.js
+++ js/main.js
@@ -44,3 +44,41 @@ switch (num){
             console.log('неизвестное число')
             break;
 }
+
+// homework 
+
+let s = 30;
+let s1 = 50;
+let s2 = 40;
+
+let s_sum = (s + s1 + s2) / 3;
+
+let m = 30;
+let m1 = 50;
+let m2 = 40;
+
+let m_sum = (s + s1 + s2) / 3;
+
+let d = 30;
+let d1 = 50;
+let d2 = 40;
+
+let d_sum = (s + s1 + s2) / 3;
+
+if (s_sum > m_sum){
+    if (s_sum > d_sum){
+        console.log('s');
+    }else if(s_sum === d_sum){
+        console.log('sd');
+    }else{
+        console.log('d');
+    }
+}else{
+    if (m_sum > d_sum){
+        console.log('m');
+    }else if(m_sum === d_sum){
+        console.log('md');
+    }else{
+        console.log('d');
+    }
+}
