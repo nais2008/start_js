39d87d04ea45cd74d9bf6fd382c596babce4d16a
nais2008 <134312352+nais2008@users.noreply.github.com>
2023-12-10 13:05:07 +0500

    vtoroi_urok_start

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-let name = 'Никита'
-let age = 15
-let history = 'Ну по идее я web-программист, но какимто боком я начал делать приложение по обработке фото на python используя PyQT5'
+//  Первый урок по JS
+// let name = 'Никита'
+// let age = 15
+// let history = 'Ну по идее я web-программист, но какимто боком я начал делать приложение по обработке фото на python используя PyQT5'
 
-confirm("Да")
-console.log(`Имя: ${name}`)
-console.log(`Возраст: ${age}`)
-console.log(`История: ${history}`)
+// confirm("Да")
+// console.log(`Имя: ${name}`)
+// console.log(`Возраст: ${age}`)
+// console.log(`История: ${history}`)
+
+// Второй урок по JS
