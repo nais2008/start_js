24bb34c19a3c5c2b0679fad35dd7dee5eb31ef40
nais2008 <134312352+nais2008@users.noreply.github.com>
2023-12-16 17:17:54 +0500

    very_stranno

--- js/main.js
+++ js/main.js
@@ -12,58 +12,58 @@
 
 // Второй урок по JS
 
-let mark = 3;
+// let mark = 3;
 
-if ((mark == 2) || (mark == 3)){
-    console.log('вы получили плохую оценку');
-}else if ((mark == 4) || (mark == 5)){
-    console.log('вы получили хоршую оценку');
-}else{
-    console.log('кажется, кто-то остался без майнкрафта на наделю');
-}
+// if ((mark == 2) || (mark == 3)){
+//     console.log('вы получили плохую оценку');
+// }else if ((mark == 4) || (mark == 5)){
+//     console.log('вы получили хоршую оценку');
+// }else{
+//     console.log('кажется, кто-то остался без майнкрафта на наделю');
+// }
 
 // ------------------------------------------------------
 
-let num = 0;
+// let num = 0;
 
-switch (num){
-    case 0:
-        console.log('это 0');
-        break;
+// switch (num){
+//     case 0:
+//         console.log('это 0');
+//         break;
 
-        case 1:
-            console.log('это 1');
-            break;
+//         case 1:
+//             console.log('это 1');
+//             break;
 
-        case 2:    
-        case 3:
-            console.log('это 2 или 3');
-            break;
+//         case 2:    
+//         case 3:
+//             console.log('это 2 или 3');
+//             break;
 
-        default:
-            console.log('неизвестное число')
-            break;
-}
+//         default:
+//             console.log('неизвестное число')
+//             break;
+// }
 
 // homework 
 
-let s = 30;
-let s1 = 50;
-let s2 = 40;
+let s = 14;
+let s1 = 29;
+let s2 = 80;
 
-let s_sum = (s + s1 + s2) / 3;
+let s_sum = (s + s1 + s2) / 3;//41
 
-let m = 30;
-let m1 = 50;
-let m2 = 40;
+let m = 43;
+let m1 = 27;
+let m2 = 2;
 
-let m_sum = (s + s1 + s2) / 3;
+let m_sum = (s + s1 + s2) / 3;//24
 
-let d = 30;
-let d1 = 50;
-let d2 = 40;
+let d = 7;
+let d1 = 24;
+let d2 = 72;
 
-let d_sum = (s + s1 + s2) / 3;
+let d_sum = (s + s1 + s2) / 3;//34
 
 if (s_sum > m_sum){
     if (s_sum > d_sum){
@@ -73,7 +73,7 @@ if (s_sum > m_sum){
     }else{
         console.log('d');
     }
-}else{
+}else if (m_sum > s_sum){
     if (m_sum > d_sum){
         console.log('m');
     }else if(m_sum === d_sum){
@@ -81,4 +81,6 @@ if (s_sum > m_sum){
     }else{
         console.log('d');
     }
+}else{
+    console.log('nich');
 }
